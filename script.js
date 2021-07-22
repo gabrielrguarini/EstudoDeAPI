61223aa02a94bb1667a1126e16f7cb11887b3571
Gabriel Guarini <gabrielrguarini@gmail.com>
2021-07-22 15:30:52 -0300

    Projeto Upado

---
+++ script.js
@@ -0,0 +1,60 @@
+document.querySelector('.busca').addEventListener('submit', async (event)=>{
+    event.preventDefault();
+
+    let input = document.querySelector('#searchInput').value;
+
+    if (input!==''){
+        clearInfo();
+        showWarning('Carregando...');
+
+
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=b608f594ddb9fdc167dd5fcae1c98b12&units=metric&lang=pt_br`;
+
+        let results = await fetch(url);
+
+        let json  = await results.json();
+
+        if (json.cod === 200) {
+            showInfo({
+                name: json.name,
+                country: json.sys.country,
+                temp: json.main.temp,
+                tempIcon: json.weather[0].icon,
+                windSpeed: json.wind.speed,
+                windAngle: json.wind.deg
+            });
+        }else{
+            clearInfo();
+            showWarning('Não encontramos a cidade');
+        }
+
+        console.log(json)
+    }else{
+        clearInfo();
+    }
+
+});
+
+function showInfo(json){
+    showWarning('');
+
+    
+    document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country}`;
+    document.querySelector('.tempInfo').innerHTML = `<sup>${json.temp}ºC</sup>`;
+    document.querySelector('.ventoInfo').innerHTML = `<span>${json.windSpeed}km/h</span>`;
+    document.querySelector('.temp > img').setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`)
+    document.querySelector('.ventoPonto').style.transform = `rotate(${json.windAngle-90}deg)`;
+    document.querySelector('.resultado').style.display = 'block';
+
+
+    
+}
+
+function showWarning(msg){
+    document.querySelector('.aviso').innerHTML = msg;
+}
+
+function clearInfo(){
+    showWarning('')
+    document.querySelector('.resultado').style.display = 'none';
+}
